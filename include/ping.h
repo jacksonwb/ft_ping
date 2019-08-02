5ddef6e1ef7bc3a50c307babe5c93bc3820cdc08
jacksonwb <jbeall@e1z3r1p3.42.us.org>
2019-08-01 21:50:25 -0700

    add icmp_send and echo functions

--- include/ping.h
+++ include/ping.h
@@ -6,7 +6,7 @@
 /*   By: jbeall <jbeall@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/30 20:16:05 by jbeall            #+#    #+#             */
-/*   Updated: 2019/07/31 19:10:28 by jbeall           ###   ########.fr       */
+/*   Updated: 2019/08/01 21:30:30 by jbeall           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,54 @@
 
 # include <unistd.h>
 # include <stdio.h>
+# include <signal.h>
 # include <sys/socket.h>
 # include <netinet/ip.h>
+# include <netinet/ip_icmp.h>
+# include <netdb.h>
 # include <arpa/inet.h>
 # include <limits.h>
+# include <time.h>
 # include "libft.h"
 
 # define OPT_V 1
 # define OPT_H 2
 # define OPT_C 4
 
+volatile int g_loop;
+
 typedef	struct	s_ping
 {
 	int			opts;
 	int			count;
+	int			seq;
 }				t_ping;
+
+# define PING_TTL 64
+# define PING_TIMEOUT 1
+
+typedef int t_icmp_sock;
+
+typedef struct			s_host
+{
+	char				*hostname;
+	char				*ipstring;
+	struct sockaddr_in	addr;
+}						t_host;
+
+# define PKT_SIZE 64
+
+typedef struct 	s_echo_req
+{
+	struct icmp hdr;
+	char msg[PKT_SIZE - sizeof(struct icmp)];
+}				t_echo_req;
+
+typedef struct	s_echo_res
+{
+	struct ip ip_hdr;
+	struct icmp hdr;
+}				t_echo_res;
+
 
 # endif
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: jbeall <jbeall@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/30 21:23:54 by jbeall            #+#    #+#             */
-/*   Updated: 2019/07/31 21:40:00 by jbeall           ###   ########.fr       */
+/*   Updated: 2019/08/01 21:49:18 by jbeall           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,12 @@ void	err_exit(char *str)
 {
 	perror(str);
 	exit(EXIT_FAILURE);
+}
+
+void	usage(void)
+{
+	PRINT("./ft_ping [-huv] host\n");
+	exit(EXIT_SUCCESS);
 }
 
 void parse_args(int ac, char **av, t_ping *opts)
@@ -43,14 +49,158 @@ void parse_args(int ac, char **av, t_ping *opts)
 	}
 }
 
-int main(int ac, char **av)
+t_icmp_sock icmp_sock__new(int ttl)
 {
-	int sfd;
-	t_ping opts;
+	t_icmp_sock sfd;
+	struct timeval tvout;
 
-	parse_args(ac, av, &opts);
 	if ((sfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP)) == -1)
 		err_exit("socket");
-	printf("Success!\n");
+	if(setsockopt(sfd, IPPROTO_IP, IP_TTL, &ttl, sizeof(ttl)))
+		err_exit("setsockopt");
+	tvout = (struct timeval){
+		.tv_sec = PING_TIMEOUT,
+		.tv_usec = 0
+	};
+	if(setsockopt(sfd, SOL_SOCKET, SO_RCVTIMEO, (const void*)&tvout, sizeof(tvout)))
+		err_exit("setsockopt");
+	return (sfd);
+}
 
+int icmp_sock__close(t_icmp_sock sfd)
+{
+	if (close(sfd) < 0)
+		return (-1);
+	return (0);
+}
+
+int icmp_socket__send(t_echo_req *req, t_icmp_sock sfd, t_host *host)
+{
+	ssize_t ret;
+
+	ret = sendto(sfd, req, sizeof(t_echo_req), 0, (struct sockaddr*)&(host->addr), sizeof(host->addr));
+	if (ret < 0 || ret != sizeof(t_echo_req))
+		return (-1);
+	return(0);
+}
+
+int host__init(t_host *host, char *hoststring)
+{
+	struct addrinfo hints;
+	struct addrinfo *addrs;
+	char hostlookup[NI_MAXHOST];
+	int ret;
+
+	ft_memset(&hints, 0, sizeof(hints));
+	ft_memset(hostlookup, 0, sizeof(hostlookup));
+	hints.ai_family = AF_INET;
+	hints.ai_flags = AI_CANONNAME;
+	if ((ret = getaddrinfo(hoststring, NULL, &hints, &addrs)))
+	{
+		printf("ft_ping: %s: %s\n", hoststring, gai_strerror(ret));
+		return (-1);
+	}
+	ft_memcpy(&(host->addr), addrs->ai_addr, sizeof(struct sockaddr));
+	host->hostname = hoststring;
+
+	if ((ret = getnameinfo((const struct sockaddr *)&(host->addr), sizeof(host->addr), hostlookup, sizeof(hostlookup), NULL, 0, NI_NUMERICHOST)))
+	{
+		printf("ft_ping: %s: %s\n", hoststring, gai_strerror(ret));
+		return (-1);
+	}
+	host->ipstring = ft_strdup(hostlookup);
+	printf("%s\n", host->ipstring);
+	return (0);
+}
+
+int echo__new(t_echo_req *req)
+{
+	ft_memset(req, 0, sizeof(t_echo_req));
+	req->hdr = (struct icmp){
+		.icmp_type = 0,
+		.icmp_cksum = 0,
+		.icmp_id = htonl(getpid())
+	};
+	ft_memset(req->msg, 0, sizeof(req->msg));
+	return (0);
+}
+
+int echo__set_seq(t_echo_req *req, int seq)
+{
+	if (seq > SHRT_MAX)
+		return (-1);
+	req->hdr.icmp_seq = htons((short)seq);
+	return (0);
+}
+
+void echo__set_checksum(t_echo_req *req)
+{
+	register long sum;
+	int count;
+	void *addr;
+
+	req->hdr.icmp_cksum = 0;
+	count = sizeof(req->hdr);
+	addr = (void*)(&(req->hdr));
+	sum = 0;
+	while( count > 1 )  {
+			sum += *(unsigned short*)addr++;
+			count -= 2;
+	}
+	if( count > 0 )
+			sum += * (unsigned char *) addr;
+	while (sum>>16)
+		sum = (sum & 0xffff) + (sum >> 16);
+	req->hdr.icmp_cksum = ~sum;
+}
+
+
+void sig_int(int s)
+{
+	(void)s;
+	printf("Recieved SIGINT\n");
+	g_loop = 0;
+}
+
+
+int ping(t_ping *opts, t_icmp_sock sfd, t_host *host)
+{
+	t_echo_req req;
+	struct timespec now;
+
+	(void)opts;
+	(void)sfd;
+	(void)host;
+
+	g_loop = 1;
+	signal(SIGINT, sig_int);
+	opts->seq = 0;
+	if (echo__new(&req))
+		return (-1);
+	if (echo__set_seq(&req, 0))
+		return (-1);
+	echo__set_checksum(&req);
+	while(g_loop)
+	{
+		if (clock_gettime(CLOCK_MONOTONIC, &now))
+			return (-1);
+	}
+	return (0);
+}
+
+int main(int ac, char **av)
+{
+	t_icmp_sock sfd;
+	t_ping opts;
+	t_host host;
+
+	if (ac < 2)
+		usage();
+	parse_args(ac, av, &opts);
+	sfd = icmp_sock__new(PING_TTL);
+
+	if (host__init(&host, av[optind]))
+		exit(EXIT_FAILURE);
+
+	return (ping(&opts, sfd, &host));
 }
